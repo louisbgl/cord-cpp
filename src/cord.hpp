b31905d9d8024bc2071de84a8ff9e1b0e965f5cc
Louis <louis.bagnol@epitech.eu>
2026-07-14 12:50:38 +0200

    feat: add macro to make linters happy about unused includes

--- src/cord.hpp
+++ src/cord.hpp
@@ -11,4 +11,8 @@ inline Schema makeSchema() {
     return Schema();
 }
 
+// If you want to make linters happy about unused includes, you can call this macro in your main() function.
+// Intentional no-op
+#define CORD_MAKE_LINTER_HAPPY() ((void)0)
+
 } // namespace cord
