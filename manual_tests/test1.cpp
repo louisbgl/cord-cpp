84d4e95836f72ae73358a8594584a8107402ac0b
Louis <louis.bagnol@epitech.eu>
2026-07-16 00:00:18 +0200

    test: add configuration file and update parsing method in test1.cpp

--- manual_tests/test1.cpp
+++ manual_tests/test1.cpp
@@ -1,26 +1,18 @@
-#include "../src/cord.hpp"
+#include "../cord.hpp"
 
 #include <iostream>
 
 int main() {
     cord::Schema schema;
-    schema.setAllowComments(false);
+    schema.setAllowComments(true);
     schema.add<int>("port").required();
     schema.add<std::string>("host").default_("localhost");
     schema.add<bool>("debug").default_(false);
 
     schema.describe();
+    std::cout << std::endl;
 
-    std::string input = R"(port=8080
-debug=true
-# hi from comment
-host="localhost"
-wello
-)";
-
-    std::cout << "\nParsing input:\n" << input << std::endl;
-
-    auto result = schema.parse(input);
+    auto result = schema.parseFile("manual_tests/test1.conf");
     if (result.hasErrors()) result.printErrors();
     else {
         std::cout << "Parsed values:" << std::endl;
