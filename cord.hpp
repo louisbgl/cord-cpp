5f14b4cc81aaae4182349396d686c1e87bc52ebc
Louis <louis.bagnol@epitech.eu>
2026-07-16 00:20:30 +0200

    chore: add gen'd single header cord.hpp

--- cord.hpp
+++ cord.hpp
@@ -57,6 +57,13 @@ public:
 
     template<typename T>
     T as() const {
+        static_assert(
+            std::is_same_v<T, int> ||
+            std::is_same_v<T, double> ||
+            std::is_same_v<T, bool> ||
+            std::is_same_v<T, std::string>,
+            "\n\n[CORD] Unsupported type for Value::as<T>()\n[CORD] Supported types: int, double, bool, std::string\n"
+        );
         return std::get<T>(_value);
     }
 
@@ -352,6 +359,13 @@ public:
 
     template<typename T>
     Field<T>& add(std::string name) {
+        static_assert(
+            std::is_same_v<T, int> ||
+            std::is_same_v<T, double> ||
+            std::is_same_v<T, bool> ||
+            std::is_same_v<T, std::string>,
+            "\n\n[CORD] Unsupported type for schema.add<T>()\n[CORD] Supported types: int, double, bool, std::string\n"
+        );
         auto field = std::make_unique<Field<T>>(name);
         Field<T>& ptr = *field;
         _fields.push_back(std::move(field));
