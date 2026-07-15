d051ed9a0b52095a462c5410154901008275a0ad
Louis <louis.bagnol@epitech.eu>
2026-07-15 18:59:33 +0200

    refactor: remove useless linter purposed stuff

--- src/cord.hpp
+++ src/cord.hpp
@@ -3,16 +3,3 @@
 // This is simply an umbrella header
 
 #include "schema.hpp"
-
-namespace cord {
-
-// Convenience, also makes linters happy about unused includes
-inline Schema makeSchema() {
-    return Schema();
-}
-
-// If you want to make linters happy about unused includes, you can call this macro in your main() function.
-// Intentional no-op
-#define CORD_MAKE_LINTER_HAPPY() ((void)0)
-
-} // namespace cord
