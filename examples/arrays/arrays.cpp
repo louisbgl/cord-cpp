c267069cf207a1f11f4113fcf9198d96ea442ccf
Louis <louis.bagnol@epitech.eu>
2026-07-21 16:08:03 +0200

    refactor: remove outdated test2 files and add arrays example

--- examples/arrays/arrays.cpp
+++ examples/arrays/arrays.cpp
@@ -1,18 +1,27 @@
-#include "../src/cord.hpp"
+#include "../../cord.hpp"
 
 #include <iostream>
+
+// I recommend peeking at examples/simplest/simplest.cpp first :)
+
+// I'll note here that we only support arrays of primitives, meaning no arrays of arrays
+// This is type checked at compile time
+// All user-facing methods that are templated do compile time checks on the types passed in for safety
 
 int main() {
     cord::Schema schema;
     schema.setAllowComments(true);
+
     schema.add<std::string>("name").required();
     schema.add<int>("level").default_(1);
+
+    // Here we say the default for the "items" field is an empty vector of strings
     schema.add<std::vector<std::string>>("items").default_({});
 
     schema.describe();
     std::cout << std::endl;
 
-    auto result = schema.parseFile("manual_tests/test2.conf");
+    auto result = schema.parseFile("examples/arrays/arrays.conf");
     if (result.hasErrors()) result.printErrors();
     else {
         std::cout << "Parsed values:" << std::endl;
