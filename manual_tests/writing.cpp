7e05b18ee196a077a726505521d4a1774474f456
Louis <louis.bagnol@epitech.eu>
2026-07-28 13:26:43 +0200

    chore: update writing manual test to use result.write()

--- manual_tests/writing.cpp
+++ manual_tests/writing.cpp
@@ -25,17 +25,12 @@ int main() {
     
     std::cout << "Parsed values:" << std::endl;
 
-    std::cout << "port: " << result.get("port").as<int>()                         << std::endl;
-    std::cout << "host: " << result.get("host").as<std::string>()                 << std::endl;
-    std::cout << "debug: " << (result.get("debug").as<bool>() ? "true" : "false") << std::endl;
-    std::cout << std::endl;
+    std::cout << result.write() << std::endl << std::endl;
 
     std::cout << "After modifications:" << std::endl;
     result.set("port", 6767).set("debug", false);
 
-    std::cout << "port: " << result.get("port").as<int>()                         << std::endl;
-    std::cout << "host: " << result.get("host").as<std::string>()                 << std::endl;
-    std::cout << "debug: " << (result.get("debug").as<bool>() ? "true" : "false") << std::endl;
+    std::cout << result.write() << std::endl;
 
     return 0;
 }
